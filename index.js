1518ed07dc71f90419d6aca61bdd137b04d363ae
smithnaomi <88051105+smithnaomi@users.noreply.github.com>
2022-01-03 18:27:13 -0500

    create and init

--- index.js
+++ index.js
@@ -72,3 +72,88 @@ const internQuestions = [
     name: "school",
   },
 ];
+
+const employeeType = [
+  {
+    type: "list",
+    message: "What type of Emplyee are you adding?",
+    name: "type",
+    choices: ["Engineer", "Intern", "No more"],
+  },
+];
+
+function createEmployee() {
+  inquirer.prompt(employeeType).then((response) => {
+    if (response.type == "Engineer") {
+      createEngineer();
+    } else if (response.type == "Intern") {
+      createIntern();
+    } else {
+      createWeb();
+    }
+  });
+}
+
+function createEngineer() {
+  inquirer.prompt(engineerQuestions).then((response) => {
+    const engineer = new Engineer(
+      response.name,
+      response.id,
+      response.email,
+      response.github
+    );
+    employees.push(engineer);
+    createEmployee();
+  });
+}
+
+function createIntern() {
+  inquirer.prompt(internQuestions).then((response) => {
+    const intern = new Intern(
+      response.name,
+      response.id,
+      response.email,
+      response.school
+    );
+    employees.push(intern);
+    createEmployee();
+  });
+}
+
+function createWeb() {
+  let body = "";
+  employees.forEach((element) => {
+    const card = new template.Card(
+      element.getName(),
+      element.getRole(),
+      element.getId(),
+      element.getEmail(),
+      Object.entries(element)[3][0],
+      Object.entries(element)[3][1]
+    );
+    body = body.concat(card.getBody());
+  });
+  fs.writeFile(
+    `dist/myteam.html`,
+    template.head.concat(body.concat(template.foot)),
+    function (err) {
+      if (err) throw err;
+      console.log("created!");
+    }
+  );
+}
+
+function init() {
+  inquirer.prompt(managerQuestions).then((response) => {
+    const manager = new Manager(
+      response.name,
+      response.id,
+      response.email,
+      response.office
+    );
+    employees.push(manager);
+    createEmployee();
+  });
+}
+
+init();
