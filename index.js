02f3945843f005eb050a6d28a3807400e0b365c7
smithnaomi <88051105+smithnaomi@users.noreply.github.com>
2022-01-02 22:28:52 -0500

    questions

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const template = require("./src/pagetemplate");
+const fs = require("fs");
+
+const engineerQuestions = [];
+const managerQuestions = [];
+const internQuestions = [];
