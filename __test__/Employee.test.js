1f9734625fc1d0318a9bee1e48d323f56f50cd73
smithnaomi <88051105+smithnaomi@users.noreply.github.com>
2022-01-04 20:25:36 -0500

    added role object, spell check

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,28 +1,29 @@
 const Employee = require("../lib/Employee");
+
 test("Can instantiate employee instance", () => {
   const e = new Employee();
   expect(typeof e).toBe("object");
 });
 
-test("Can name be set via these agruments?", () => {
+test("Can name be set via these arguments?", () => {
   const name = "Susan Tart";
   const e = new Employee(name);
   expect(e.name).toBe(name);
 });
 
-test("Can set employee id via these agruments", () => {
+test("Can set employee id via these arguments", () => {
   const id = 222;
   const e = new Employee("Susan Tart", 444);
   expect(e.id).toBe(id);
 });
 
-test("Can set employee email via these agruments", () => {
+test("Can set employee email via these arguments", () => {
   const email = "tartsusan@gmail.com";
   const e = new Employee("Susan Tart", 444, email);
   expect(e.email).toBe(email);
 });
 
-test("Can set employee role via these agruments", () => {
+test("Can set employee role via these arguments", () => {
   const role = "Employee";
   const e = new Employee("Susan Tart", 444, "tartsusan@gmail.com", role);
   expect(e.role).toBe(role);
