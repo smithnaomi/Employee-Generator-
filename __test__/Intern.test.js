94235cfbaec2468f7055641a08b19c5d3861a4fd
smithnaomi <88051105+smithnaomi@users.noreply.github.com>
2022-01-08 23:54:17 -0500

    spell check

--- __test__/Intern.test.js
+++ __test__/Intern.test.js
@@ -5,31 +5,31 @@ test("Can instantiate Intern instance", () => {
   expect(typeof e).toBe("object");
 });
 
-test("Can set Intern name via these agruments", () => {
+test("Can set Intern name via these arguments", () => {
   const name = "Susan Tart";
   const e = new Intern(name);
   expect(e.name).toBe(name);
 });
 
-test("Can set Intern id via these agruments", () => {
+test("Can set Intern id via these arguments", () => {
   const id = 444;
   const e = new Intern("Susan Tart", 444);
   expect(e.id).toBe(id);
 });
 
-test("Can set Intern email via these agruments", () => {
+test("Can set Intern email via these arguments", () => {
   const email = "tartsusan@gmail.com";
   const e = new Intern("Susan Tart", 444, email);
   expect(e.email).toBe(email);
 });
 
-test("Can set Intern school via these agruments", () => {
+test("Can set Intern school via these arguments", () => {
   const school = "North Carolina State University";
   const e = new Intern("Susan Tart", 444, "tartsusan@gmail.com", school);
   expect(e.school).toBe(school);
 });
 
-test("Can set Intern role via these agruments", () => {
+test("Can set Intern role via these arguments", () => {
   const role = "Intern";
   const e = new Intern(
     "Susan Tart",
