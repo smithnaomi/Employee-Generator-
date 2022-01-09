d7d930530a651289b84cf102698303e2599fc588
smithnaomi <88051105+smithnaomi@users.noreply.github.com>
2022-01-08 23:57:07 -0500

    spell check

--- __test__/Manager.test.js
+++ __test__/Manager.test.js
@@ -5,31 +5,31 @@ test("Can instantiate Manager instance", () => {
   expect(typeof e).toBe("object");
 });
 
-test("Can set Manager name via these agruments", () => {
+test("Can set Manager name via these arguments", () => {
   const name = "Susan Tart";
   const e = new Manager(name);
   expect(e.name).toBe(name);
 });
 
-test("Can set Manager id via these agruments", () => {
+test("Can set Manager id via these arguments", () => {
   const id = 444;
   const e = new Manager("Susan Tart", 444);
   expect(e.id).toBe(id);
 });
 
-test("Can set Manager email via these agruments", () => {
+test("Can set Manager email via these arguments", () => {
   const email = "tartsusan@gmail.com";
   const e = new Manager("Susan Tart", 444, email);
   expect(e.email).toBe(email);
 });
 
-test("Can set manager office number via these agruments", () => {
+test("Can set manager office number via these arguments", () => {
   const officeNumber = 674587;
   const e = new Manager("Susan Tart", 444, "tartsusan@gmail.com", officeNumber);
   expect(e.officeNumber).toBe(officeNumber);
 });
 
-test("Can set manager role via these agruments", () => {
+test("Can set manager role via these arguments", () => {
   const role = "Manager";
   const e = new Manager("Susan Tart", 444, "tartsusan@gmail.com", 674587, role);
   expect(e.role).toBe(role);
